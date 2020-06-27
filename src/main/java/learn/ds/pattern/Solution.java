5b2c70b5ef01fd541c78c2b6fc8593f51e169ac6
mac <kumaranmani92@gmail.com>
2020-06-28 04:52:33 +0530

    Pattern

---
+++ src/main/java/learn/ds/pattern/Solution.java
@@ -0,0 +1,16 @@
+package learn.ds.pattern;
+
+public class Solution{
+	public static void main(String as[]){
+		//hard coded test cases
+		int input = 3;
+		
+		for(int outer_i = input ; outer_i > 0 ; outer_i-- ) {
+			int temp = input+1;
+			for(int inner_i = input*outer_i ; inner_i > 0 ; inner_i-- ) {
+				if(inner_i%outer_i==0)temp--;
+				System.out.print(temp+" ");
+			}System.out.print("*");
+		}
+	}
+}
