27827d6f2ead6698be668f6b581f44d3bba098ed
mac <kumaranmani92@gmail.com>
2020-06-29 11:25:51 +0530

    tested

--- src/main/java/learn/ds/recursion/Solution.java
+++ src/main/java/learn/ds/recursion/Solution.java
@@ -3,31 +3,35 @@ package learn.ds.recursion;
 import java.util.Arrays;
 
 public class Solution{
-
-	static int[][] arr = new int[5][5];
+	
 	public static void main(String[] args) {
 		
-		arr[0] = new int[]{0,1,1,0,1}; 
-		arr[1] = new int[]{3,1,1,4,6};
-		arr[2] = new int[]{7,8,1,1,3};
-		arr[3] = new int[]{5,1,1,1,2};
-		arr[4] = new int[]{3,4,7,1,1};
-		int k = 9;
-		int x = 1,y=1;
+		int arr[][] = {{1, 1, 1, 1, 1, 1, 1, 1}, 
+                {1, 1, 1, 1, 1, 1, 0, 0}, 
+                {1, 0, 0, 1, 1, 0, 1, 1}, 
+                {1, 2, 2, 2, 2, 0, 1, 0}, 
+                {1, 1, 1, 2, 2, 0, 1, 0}, 
+                {1, 1, 1, 2, 2, 2, 2, 0}, 
+                {1, 1, 1, 1, 1, 2, 1, 1}, 
+                {1, 1, 1, 1, 1, 2, 2, 1}, 
+                };
+		int k = 3;
+		int x = 4,y=4;
 		int v = arr[x][y];
 		
-		rec(v,k,x,y);
+		rec(arr,v,k,x,y);
 		for(int[] ar : arr)
 			System.out.println(Arrays.toString(ar)); 
 	}
-	public static void rec(int v,int k,int x, int y) {
+
+	public static void rec(int[][] arr,int v,int k,int x, int y) {
 		if((x==-1 || y==-1 || x>=arr.length || y>=arr[0].length) || arr[x][y]!=v)return;
 		
 		arr[x][y]=k;
 		
-		rec(v,k,x,y-1);
-		rec(v,k,x-1,y);
-		rec(v,k,x+1,y);
-		rec(v,k,x,y+1);
+		rec(arr,v,k,x,y-1);
+		rec(arr,v,k,x-1,y);
+		rec(arr,v,k,x+1,y);
+		rec(arr,v,k,x,y+1);
 	} 
 }
